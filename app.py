e52844a3338769876b4dc372b6526e2787a6ea91
gokul <gkannan931@gmail.com>
2019-02-23 15:26:59 +0530

    my flask are commit

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return"hello....."
+    return render_template("index.html")
 
 @app.route("/home")
 def home():
-    return"My Home Page"
+    return render_template("art.html")
 @app.route("/contact")
 def contact():
-    return"My contact Page"
+    return render_template("art.html")
 
 if(__name__=="__main__"):
-    app.run()
+    app.run(debug=True)
